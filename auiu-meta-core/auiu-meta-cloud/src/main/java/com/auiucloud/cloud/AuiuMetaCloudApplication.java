962bdbb0dfcd9fd7d66ba4928b8e6af94505c7aa
dries <1451691457@qq.com>
2021-12-17 19:46:55 +0800

    配置打包上传

---
+++ auiu-meta-core/auiu-meta-cloud/src/main/java/com/auiucloud/cloud/AuiuMetaCloudApplication.java
@@ -0,0 +1,17 @@
+package com.auiucloud.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dries
+ * @date 2021/12/17
+ */
+@SpringBootApplication
+public class AuiuMetaCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuiuMetaCloudApplication.class, args);
+    }
+
+}
