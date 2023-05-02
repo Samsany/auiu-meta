f0b9c083002cd40a419797f5925a774717c68a2b
dries <1451691457@qq.com>
2023-05-02 12:47:20 +0800

    feat: 升级spring boot3.0

---
+++ auiu-meta-core/auiu-meta-auth/src/test/java/com/auiucloud/core/auth/AuiuMetaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auiucloud.core.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuiuMetaAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
